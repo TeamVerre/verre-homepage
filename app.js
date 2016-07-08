4859d1240b3f9a650502e90c1b4508856a97261a
Alex Motz <alexmotz@me.com>
2016-07-08 18:57:17 -0400

    Eric edits

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 var PORT = 3000;
 
-app.use('/', express.static('public'));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, function () {
   console.log('Verre Homepage - listening on port: ', PORT);
