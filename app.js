9609ecc4d13f2fe9f4e532e3580df85f21904430
Alex Motz <alexmotz@me.com>
2016-07-08 19:05:46 -0400

    revert and added ../

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 var PORT = 3000;
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static('../public'));
 
 app.listen(PORT, function () {
   console.log('Verre Homepage - listening on port: ', PORT);
